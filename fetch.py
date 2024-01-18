a0a5110dc495c693dafb586946948eb8193ec82f
Ori Donner <ori.donner@perception-point.io>
2024-01-18 11:10:24 +0200

    create exporter.yaml file

--- fetch.py
+++ fetch.py
@@ -6,6 +6,7 @@ from opensearchpy.exceptions import AuthorizationException
 from datetime import datetime, timedelta
 from requests_aws4auth import AWS4Auth
 from flask import Flask, jsonify, g
+
 import boto3
 import logging
 import sys
@@ -22,39 +23,33 @@ QUERY_TIME_RANGE = int(os.getenv('QUERY_TIME_RANGE', 60)) # minutes
 QUERY_TIME_LAG = int(os.getenv('QUERY_TIME_LAG', 30)) # minutes
 QUERY_TIME_REFRESH = int(os.getenv('QUERY_TIME_REFRESH', 60)) # seconds
 
-global session, awsauth
+# App Logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# OpenSearch Client
+global awsauth, session, credentials, client, query, opensearch_index, opensearch_host, opensearch_port
 opensearch_index = os.getenv('opensearch_index','ops-logs-*')
 opensearch_host = os.getenv('opensearch_host','logs.testing.perception-point.io')
 opensearch_port = int(os.getenv('opensearch_port',443))
-service = 'es'
+session = boto3.Session()
+credentials = session.get_credentials()
+print(f"============================== Initial Access Key: {credentials.access_key} ================================")
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, session.region_name, 'es', session_token=credentials.token)
+client = OpenSearch(
+    hosts=[{'host': opensearch_host, 'port': opensearch_port}],
+    use_ssl=True,
+    http_auth=awsauth,
+    verify_certs=True,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False,
+    connection_class=RequestsHttpConnection
+)
 start_time = datetime.utcnow() - timedelta(minutes=QUERY_TIME_LAG+QUERY_TIME_RANGE)
 end_time = datetime.utcnow() - timedelta(minutes=QUERY_TIME_LAG)
 start_time_str = start_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
 end_time_str = end_time.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-labels = [
-    'unpack',
-    'scanning',  
-    'start_remote_scan', 
-    'scan_duration',
-    '_static_scan_and_unpacking',
-    'extract_scan',
-    'extract_scan.WeTransferDownloadURLExtractor',
-    'browse_and_wait_until_loaded',
-    'extract_scan.GoogleSpreadsheetsDownloadURLExtractor',
-    'extract_scan.HtmlDynamicEvidenceExtractor',
-    'extract_scan.TwoStepExtractor',
-    'extract_scan.GoogleDocsDownloadURLExtractor',
-    'extract_scan.GenericSeleniumCrawlerExtractor',  
-    '_create_chrome_driver',
-    'run_decisions',
-    'scanner.URLScreenshotScanner',
-    'extract_scan.GoogleDriveDownloadURLExtractor',
-    'extract_scan.URLScreenshotExtractor',
-    'extract_scan.SharepointDownloadExtractor',
-    'extract_scan.FaviconExtractor'
-]
+
 query = {
     "aggs": {
         "2": {
@@ -118,80 +113,45 @@ query = {
         }
     }
 }
+
+# Flask
 app = Flask(__name__)
 
+# Functions
 def refresh_aws_credentials():
     try:
         session = boto3.Session()
         credentials = session.get_credentials()
-        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, session.region_name, service, session_token=credentials.token)
+        print(f"============================== Refreshed Access Key: {credentials.access_key} ================================")
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, session.region_name, 'es', session_token=credentials.token)
         client = OpenSearch(
             hosts=[{'host': opensearch_host, 'port': opensearch_port}],
-            http_auth=awsauth,
             use_ssl=True,
+            http_auth=awsauth,
             verify_certs=True,
             ssl_assert_hostname=False,
             ssl_show_warn=False,
             connection_class=RequestsHttpConnection
         )
-        # result = client.search(index=opensearch_index, body=query)
         logger.info("AWS credentials refreshed successfully.")
-    
+        fetch_data()
     except (NoCredentialsError, PartialCredentialsError) as e:
         logger.error(f"Error refreshing AWS credentials: {e}")
 
 def fetch_data():
-    try:
-        session = boto3.Session()
-        credentials = session.get_credentials()
-        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, session.region_name, service, session_token=credentials.token)
-        client = OpenSearch(
-            hosts=[{'host': opensearch_host, 'port': opensearch_port}],
-            use_ssl=True,
-            http_auth=awsauth,
-            verify_certs=True,
-            ssl_assert_hostname=False,
-            ssl_show_warn=False,
-            connection_class=RequestsHttpConnection
-        )
-        print(query)
-        result = client.search(index=opensearch_index, body=query)
-        print(result)
-        buckets = result['aggregations']['2']['buckets']
-        for bucket in buckets:
-            function_label_key = bucket['key']
-            if function_label_key in labels:
-            #     # Average
-                average_float_1_value = bucket['1']['value']
-                AVERAGE_FLOAT_1_GAUGE.labels(function=function_label_key).set(average_float_1_value)
-                # print(f"Function: {function_label_key}, Average float_1: {average_float_1_value}")
-            #     # Median
-                median_float_1_value = bucket['3']['values']['50.0']
-                MEDIAN_FLOAT_1_GAUGE.labels(function=function_label_key).set(median_float_1_value)
-                # print(f"Function: {function_label_key}, Median float_1: {median_float_1_value}")
-            #     # Max
-                max_float_1_value = bucket['4']['value']
-                MAX_FLOAT_1_GAUGE.labels(function=function_label_key).set(max_float_1_value)
-                # print(f"Function: {function_label_key}, Max float_1: {max_float_1_value}")
-            #    # Count
-                function_count = bucket['doc_count']
-                FUNCTION_COUNT_GUAGE.labels(function=function_label_key).set(function_count)
-                # print(f"Function: {function_label_key}, Count: {function_count}")
-        LAST_FETCH_TIME.set(int(end_time.timestamp()))
-
-    except NoCredentialsError:
-        logger.warning("AWS credentials need refreshing.")
-        refresh_aws_credentials()
-        raise
-
-    except AuthorizationException as e:
-        logger.error(f"Error: Invalid security token - {e}")
-        refresh_aws_credentials()
-        raise
-
-    except Exception as e:
-        logger.error(f"Error fetching data: {e}")
-        raise
+    result = client.search(index=opensearch_index, body=query)
+    buckets = result['aggregations']['2']['buckets']
+    for bucket in buckets:
+        function_label_key = bucket['key']
+        average_float_1_value = bucket['1']['value']
+        AVERAGE_FLOAT_1_GAUGE.labels(function=function_label_key).set(average_float_1_value)
+        median_float_1_value = bucket['3']['values']['50.0']
+        MEDIAN_FLOAT_1_GAUGE.labels(function=function_label_key).set(median_float_1_value)
+        max_float_1_value = bucket['4']['value']
+        MAX_FLOAT_1_GAUGE.labels(function=function_label_key).set(max_float_1_value)
+        function_count = bucket['doc_count']
+        FUNCTION_COUNT_GUAGE.labels(function=function_label_key).set(function_count)
+    LAST_FETCH_TIME.set(int(end_time.timestamp()))
 
 # Register the error handler
 @app.errorhandler(Exception)
@@ -200,15 +160,16 @@ def handle_error(error):
 
 @app.route('/metrics')
 def metrics():
-    REQUEST_COUNT.inc()  
-    fetch_data() 
+    REQUEST_COUNT.inc()
+    fetch_data()
     if hasattr(g, 'exception'):
-        return g.exception, 500  
+        return g.exception, 500
     else:
         return generate_latest(REGISTRY)
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(fetch_data, 'interval', seconds=QUERY_TIME_REFRESH) 
+    scheduler.add_job(refresh_aws_credentials, 'interval', minutes=50)
+    scheduler.add_job(fetch_data, 'interval', seconds=QUERY_TIME_REFRESH)
     scheduler.start()
     app.run(host='0.0.0.0', port=5000)
--- scripts/script.sh
+++ scripts/script.sh
@@ -8,3 +8,21 @@ sudo docker run \
  -e "QUERY_TIME_RANGE=5" -e "QUERY_TIME_LAG=20" -e "QUERY_TIME_REFRESH=50" \
  -p 5000:5000 fetch:1.0.3
 
+# install botosession
+pip3 install git+https://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@github.com/perceptionpoint/botosession.git
+
+# upload docker image to ECR
+sudo docker build -t fetch:1.1.0 . 
+sudo docker tag fetch:1.1.0 226228012207.dkr.ecr.us-east-1.amazonaws.com/opensearch-exporter:1.1.0
+sudo docker tag fetch:1.1.0 226228012207.dkr.ecr.us-east-1.amazonaws.com/opensearch-exporter:latest
+aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 226228012207.dkr.ecr.us-east-1.amazonaws.com
+sudo docker push 226228012207.dkr.ecr.us-east-1.amazonaws.com/opensearch-exporter:1.1.0
+sudo docker push 226228012207.dkr.ecr.us-east-1.amazonaws.com/opensearch-exporter:latest
+
+# restart deployment
+kubectl delete -f k8s/Deployment.yaml 
+kubectl apply -f k8s/Deployment.yaml 
+kubectl get pods -n mantis-system | grep opensearch
+# log into pod
+kubectl exec -it opensearch-exporter-deployment-5564ffb9f6-jfl6x -n mantis-system -- bash
+# pod logs
